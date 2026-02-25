f349036f07b4404ba7f155b774bfc25fafe6f2bd
Akmalrian <105763366+Akmalrian@users.noreply.github.com>
2026-02-25 14:27:37 +0700

    Create minitask-2.js

---
+++ minitask-2.js
@@ -0,0 +1,19 @@
+const belanja = 1200000
+
+console.log("Belanja =Rp."+belanja)
+
+if (belanja < 500000){
+    console.log("Tidak ada diskon")
+    diskon = 0
+}
+else if(belanja>= 500000 && belanja <= 1000000){
+    console.log("Diskon 5%")
+    diskon = belanja *0.05
+}
+else {
+    console.log("Diskon 10%")
+    diskon = belanja *0.1
+}
+
+totalBelanja = belanja-diskon
+console.log("Total Belanja =Rp."+totalBelanja)
