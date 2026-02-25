7d4732596085fd77909f1160048f1595f3b5dd3a
Akmalrian <105763366+Akmalrian@users.noreply.github.com>
2026-02-25 15:58:05 +0700

    Update minitask-3.js

--- minitask-3.js
+++ minitask-3.js
@@ -9,6 +9,8 @@ let uang3 = 5000
 let uang4 = 2000
 let uang5 = 1000
 
+console.log("Uang =Rp."+uang )
+console.log("Pembayaran =Rp."+pembayaran )
 
 let kembalian = uang-pembayaran
 
@@ -16,7 +18,7 @@ let kembalian = uang-pembayaran
     while(kembalian >= uang2 ){
         totalKembali = 10000
         totalKembali2 = totalKembali2 + 10000
-        console.log(totalKembali)
+        console.log("Uang Kembalian =Rp."+totalKembali)
         uang2 = uang2 + 10000
     }
     finalKembali = kembalian - totalKembali2
@@ -24,7 +26,7 @@ let kembalian = uang-pembayaran
         while(finalKembali >= uang3){
         totalKembali = 5000
         totalKembali2 = totalKembali2 + 5000
-        console.log(totalKembali)
+        console.log("Uang Kembalian =Rp."+totalKembali)
         uang3 = uang3 + 5000
         }
         finalKembali2 = kembalian - totalKembali2
@@ -32,14 +34,14 @@ let kembalian = uang-pembayaran
         while(finalKembali2 >= uang4){
         totalKembali = 2000
         totalKembali2 = totalKembali2 + 2000
-        console.log(totalKembali)
+        console.log("Uang Kembalian =Rp."+totalKembali)
         uang4 = uang4 + 2000
         }
         finalKembali3 = kembalian - totalKembali2
         while(finalKembali3 >= uang5){
         totalKembali = 1000
         totalKembali2 = totalKembali2 + 1000
-        console.log(totalKembali)
+        console.log("Uang Kembalian =Rp."+totalKembali)
         uang5 = uang5 + 1000
         }
         
